5f96412037d99ce240fb8b715d665aefbf9790fb
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2025-08-05 18:52:23 +0300

    Delete examples/Radioactive directory

--- examples/Radioactive/information/information.ino
+++
@@ -1,3 +0,0 @@
----------- For Library Information ----------
-  To estimate decay times in radioactive data science application: https://github.com/abcdaaaaaaaaa/RadioactiveDataScience/tree/main/RadioactiveScience
-  To process your Geiger Counter into the microcontroller: Radioactive.ino
