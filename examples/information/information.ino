202dc9299ee914c608de1bb22603786db8507176
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2025-09-04 16:10:32 +0300

    Update information.ino

--- examples/information/information.ino
+++ examples/information/information.ino
@@ -1,3 +1,4 @@
 ---------- For Library Information ----------
-  To estimate decay times in radioactive data science application: https://github.com/abcdaaaaaaaaa/RadioactiveDataScience/tree/main/RadioactiveScience
+  To estimate decay times in radioactive data science application: https://github.com/abcdaaaaaaaaa/RadioactiveDataScience/blob/main/RadioactiveScience
+
   To process your Geiger Counter into the microcontroller: Radioactive.ino
