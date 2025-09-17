13cb06a68f8bbe6a1f19b107d020b4dc18412f2a
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2025-09-17 17:21:41 +0300

    Update information.ino

--- examples/information/information.ino
+++ examples/information/information.ino
@@ -1,3 +1,7 @@
 ---------- For Library Information ----------
   To estimate decay times in radioactive data science application: https://github.com/abcdaaaaaaaaa/RadioactiveDataScience/tree/main/RadioactiveScience
   To process your Geiger Counter into the microcontroller: Radioactive.ino
+
+-- WARNING --
+  RadioactiveScience/Radioactive_Datas.xlsx file is required for radioactive data estimation (RadioactiveScience.py).
+  Please replace RadioactiveScience/Radioactive_Datas.xlsx with your actual measured data.
